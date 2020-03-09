f75f62b82818de74d9070b2265937f65f1da90b2
jguzz <joseph.a.guzzardo@gmail.com>
2020-03-09 10:12:57 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        "Hello, World!"
+    end
+    get '/name' do 
+        "My name is Joseph Guzzardo"
+    end
+    get '/hometown' do 
+        "My hometown is Whitestone"
+    end
+    get '/favorite-song' do
+        "My favorite song is Habits by Tove Lo"
+    end
 end
